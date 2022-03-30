440a88c92598472ffcb098a5e16d81d6711858e3
GUILHERME RODRIGUES ARAUJO <guilherme.araujo1535@gmail.com>
2022-03-30 11:35:10 -0300

    Testes CSHARP

--- CATI9101/Program.cs
+++ CATI9101/Program.cs
@@ -4,86 +4,83 @@ namespace CATI9101
 {
     internal class Program
     {
-        // Atributos
-
-        // Construtores - Criam e inicializam instancias
-        
-        // Metodos de acesso (get e set) - C# -> Propriedades
-
-        // Metodos da classe - função, operação, ação do objeto
-
-        static void Main(string[] args) // void nao retorna
+        // Main
+        static void Main(string[] args)
         {
-            /*
-            // Declarar variavel
-             var x = 10;
-             var nome = "Gui";
-             var teste = true;
-             var valor = 12.89;
-             var num = 15f;
-             var y = false;
+            //Tabuada();
+            Matriz();
 
- 
-             byte[] vetor = new byte[3]; // Matriz de bytes / byte vai de 0~255
-             vetor[0] = 71;
-             vetor[1] = 117;
-             vetor[2] = 105;
-
-             for (int i = 0; i < vetor.Length; i++)
-             {
-                 Console.WriteLine(vetor[i]);
-             }
-
-            // --------------------------------------------------------------------------------------//
-            // Variaveis
-           
-            byte l = 255; // 0~255
-            short s = 32767; // 16bits
-            ushort ss = 65535; // Valores positivos até 16bits
-            int w; //w += 1; // w = w + 1
-            double A, B, C, D;
-            A = 2.55;
-            float c = 10.66f;
-            uint ww = 2147483648;
-            ulong t = 782378217817821283; // 64^2
-            long tt = 231223131212331231; // 64^2
-            string m = "Senac";
-           // m = m.Substring(2, 1); // Recuperar letra N
-
-
-            bool chk = false;
-            if (!chk)
-            {
-for (int i = 0; i < m.Length; i++)
-            {
-
-                   // Console.WriteLine(m.Substring(i, 1));
-                }
-                chk = true;
-            }
-// --------------------------------------------------------------------------------------//
-           
-            // Formas de conversões de tipos
-
-           
-        */
-
-            Console.WriteLine("Digite seu nome");
-            string nome =Console.ReadLine();
-            Console.WriteLine("Digite o ano de seu nascimento: ");
-            // int ano = (int)Console.ReadLine(); // cast
-            // int ano = int.Parse(Console.ReadLine()); // parse
-            string ano = Console.ReadLine(); // classe convert
-            int idade = DateTime.Now.Year - int.Parse(ano);
-            Console.WriteLine("Ola {0}, sua idade é {1} anos", nome, idade);
             Console.ReadKey();
         }
+
+        static void Matriz()
+        {
+            // Declaração de matrizes
+            string[] jogadores = { "José", "Paulo", "Gabriel", "Julio", "Marcos" };
+            // int[,] pontos = { { 0, 2 }, { 1, 3 }, { 2, 5 }, { 3, 4 } };
+            string[] times = new string[5];
+            int[,] tabela = new int[5, 6];
+
+            /*
+            int[,] m = new int[4, 2];
+            m[0, 0] = 0;
+            m[1, 0] = 1;
+            m[2, 0] = 2;
+            m[3, 0] = 3;
+            m[0, 1] = 4;
+            */
+            foreach (string jogador in jogadores)
+            {
+                Console.WriteLine(jogador);
+            }
+        }
+
+
+        // Tabuada
+        /*
+        static void Tabuada()
+        {
+        int[] vnumero = new int[10];
+        string sair = string.Empty;
+        int cont = 10;
+            while (sair != "s" && cont > 0)
+            {
+                Console.WriteLine("Digite um numero de 1 a 10: ");
+
+                bool atender = false;
+                int numero = int.Parse(Console.ReadLine());
+                do
+                {
+                    if (numero > 0 && numero < 11)
+                    {
+                        for (int i = 0; i < 10; i++)
+                        {
+                            Console.WriteLine($"{numero} X {i + 1} = {numero * (i + 1)}");
+                        }
+                        atender = true;
+
+                    }
+                    else
+                    {
+                        atender = false;
+                        Console.WriteLine("Você igite um numero de 1 a 10");
+                        numero = int.Parse(Console.ReadLine());
+                    }
+                } while (!atender);
+                cont--; // decrementa um número na variável contador
+                vnumero[cont] = numero;
+
+                Console.WriteLine("Deseja sair? (s) ou enter para continuar:");
+                sair = Console.ReadLine();
+                Console.Clear(); // limpa o console 
+            }// fim do While
+            Console.WriteLine();
+            Console.Write("Você exibiu as tabuadas dos números ");
+            foreach (var item in vnumero)
+            {
+                Console.Write($"{item} | ");
+            }
+        }
+        */
     }
 }
-/*
-          Console.WriteLine("Digite seu nome: ");
-              string nome = Console.ReadLine();
-          Console.WriteLine("Bom dia " + nome);
-          Console.ReadKey();
-// Copilot
-*/
